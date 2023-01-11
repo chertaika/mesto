1e07d6cd6e816c7d3af57a9dda975ceb022ba130
Ekaterina Iurkova <chertaika1439@yandex.ru>
2023-01-12 04:04:33 +0700

    fix: исправления согласно комментариям

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
-let likeButtons = document.querySelectorAll('.elements__like');
+// let likeButtons = document.querySelectorAll('.elements__like');
+//
+// likeButtons.forEach(function (btn) {
+//   btn.addEventListener('click', function () {
+//     this.classList.toggle('elements__like_active');
+//   })
+// })
 
-likeButtons.forEach(function (btn) {
-  btn.addEventListener('click', function () {
-    this.classList.toggle('elements__like_active');
-  })
-})
-
-let modal = document.querySelector('.modal');
+let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-btn');
-let closeButton = document.querySelector('.modal__close-btn');
-let formProfile = document.querySelector('.modal__form-profile');
+let closeButton = document.querySelector('.popup__close-btn');
+let formProfile = document.querySelector('.popup__form-profile');
 let name = document.querySelector('.profile__name');
 let prof = document.querySelector('.profile__prof');
-let nameInput = document.querySelector('.modal__input_type_name');
-let profInput = document.querySelector('.modal__input_type_prof');
+let nameInput = document.querySelector('.popup__input_type_name');
+let profInput = document.querySelector('.popup__input_type_prof');
 
-function showModal() {
+function showPopup() {
   nameInput.value = name.textContent;
   profInput.value = prof.textContent;
-  modal.classList.add('modal_show');
+  popup.classList.add('popup_opened');
   setTimeout(function () {
     nameInput.focus()
   }, 30);
 }
 
-function closeModal() {
-  modal.classList.remove('modal_show');
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
 
 function saveProfile(evt) {
@@ -36,16 +36,16 @@ function saveProfile(evt) {
   }
   name.textContent = nameInput.value;
   prof.textContent = profInput.value;
-  closeModal();
+  closePopup();
 }
 
-editButton.addEventListener('click', showModal)
+editButton.addEventListener('click', showPopup)
 formProfile.addEventListener('submit', saveProfile)
-closeButton.addEventListener('click', closeModal)
-document.addEventListener('keydown', function (key) {
-  if (key.code === "Escape" && modal.classList.contains('modal_show')) {
-    closeModal();
-  }
-});
+closeButton.addEventListener('click', closePopup)
+// document.addEventListener('keydown', function (key) {
+//   if (key.code === "Escape" && popup.classList.contains('popup_show')) {
+//     closePopup();
+//   }
+// });
 
 
