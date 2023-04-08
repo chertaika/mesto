559503b5e604d9743f52d36ce8661a20f270e8f7
Ekaterina Iurkova <chertaika1439@yandex.ru>
2023-04-08 13:53:28 +0700

    правка конфигурации

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export default class Card {
-  constructor(data, templateElement, handleCardClick) {
-    this._name = data.name;
-    this._link = data.link;
+  constructor({name, link}, templateElement, handleCardClick) {
+    this._name = name;
+    this._link = link;
     this._templateElement = templateElement;
     this._handleCardClick = handleCardClick;
   }
